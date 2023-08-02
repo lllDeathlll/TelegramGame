aeef8f75249fabc2e5897e1e20e841a2dcdf5517
Roman Avdeev <lll_Death_lll@outlook.com>
2023-08-02 18:40:38 +0400

    Added Bitcoin Command

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
+import random
+import string
+import requests
 # Import token from cfg.py
 from cfg import TOKEN_API
 
@@ -6,30 +9,49 @@ from cfg import TOKEN_API
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
-'''@dp.message_handler()
-async def echo(message: types.message):
-    await message.answer(text=str(message.text).capitalize) # Mirror a message to user'''
+# Adds random letter on text
+@dp.message_handler()
+async def random(message: types.message):
+    await message.reply(text=f"{await random.choice(set([string.ascii_letters]))}")
 
-'''@dp.message_handler()
-async def echo_capitalize(message: types.message):
-    await message.answer(text=message.text.capitalize()) # Mirror a capitalized message to user'''
-    
-'''@dp.message_handler()
-async def echo_upper(message: types.message):
-    if message.text.count(' ') >= 1: # Checks if message is more than 1 word
-        await message.answer(text=message.text.upper()) # Mirror an upper message to user'''
-
-@dp.message_handler(commands=['start'])
+# Adds start command with welcome message
+@dp.message_handler(commands=["start"])
 async def help_command(message: types.message):
-    await message.answer(text='Welcome to our telegram bot!')
+    await message.answer(text="Welcome to our telegram bot!")
     await message.delete()
-        
-@dp.message_handler(commands=['help'])
+
+# Adds help command that lists all commands
+@dp.message_handler(commands=["help"])
 async def help_command(message: types.message):
     await message.reply(text="""
-    /start - start working with bot
-    /help - list of commands
-    """)
+/start - start working with bot
+/help - list of commands
+/description - gives you a description of our bot
+/bitcoin - gives you bitcoin rate
+""")
+    
+# Adds description command that gives a description of bot
+@dp.message_handler(commands=["description"])
+async def help_command(message: types.message):
+    await message.reply(text="In our bot you will be able to play a game")
+    
+# Adds bitcoin command that gives you bitcoin rate    
+@dp.message_handler(commands=['bitcoin'])
+async def help_command(message: types.message):
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    response = await requests.get(url)
+    data = response.json()
+
+    await message.reply(text=f"Bitcoin value in USD is: {data['bpi']['USD']['rate']}")
+
+count = 0
+ 
+# Adds count command that adds 1 every time
+@dp.message_handler(commands=['count'])
+async def help_command(message: types.message):
+    global count
+    await message.reply(text=f"Count is {count}")
+    count += 1
 
 if __name__ == '__main__':
     executor.start_polling(dp)
