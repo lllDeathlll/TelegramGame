02a90a8ddebd77741796643f849a406d0c9dcbaf
Roman Avdeev <lll_Death_lll@outlook.com>
2023-08-02 18:06:56 +0400

    Added Start and Help commands

--- main.py
+++ main.py
@@ -14,10 +14,22 @@ async def echo(message: types.message):
 async def echo_capitalize(message: types.message):
     await message.answer(text=message.text.capitalize()) # Mirror a capitalized message to user'''
     
-@dp.message_handler()
+'''@dp.message_handler()
 async def echo_upper(message: types.message):
     if message.text.count(' ') >= 1: # Checks if message is more than 1 word
-        await message.answer(text=message.text.upper()) # Mirror an upper message to user
+        await message.answer(text=message.text.upper()) # Mirror an upper message to user'''
+
+@dp.message_handler(commands=['start'])
+async def help_command(message: types.message):
+    await message.answer(text='Welcome to our telegram bot!')
+    await message.delete()
+        
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.message):
+    await message.reply(text="""
+    /start - start working with bot
+    /help - list of commands
+    """)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
