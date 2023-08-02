1256cdc56d52f221b72189890f2fce65a1568151
Roman Avdeev <lll_Death_lll@outlook.com>
2023-08-02 17:53:03 +0400

    Added Mirror Upper On 2 Messages

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ from cfg import TOKEN_API
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
+'''@dp.message_handler()
 async def echo(message: types.message):
-    await message.answer(text=str(message.text).capitalize) # Write message
+    await message.answer(text=str(message.text).capitalize) # Mirror a message to user'''
+
+'''@dp.message_handler()
+async def echo_capitalize(message: types.message):
+    await message.answer(text=message.text.capitalize()) # Mirror a capitalized message to user'''
+    
+@dp.message_handler()
+async def echo_upper(message: types.message):
+    if message.text.count(' ') >= 1: # Checks if message is more than 1 word
+        await message.answer(text=message.text.upper()) # Mirror an upper message to user
 
 if __name__ == '__main__':
     executor.start_polling(dp)
