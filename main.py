760c8649e7188b4f7781631e3f7b21f0cbc2ea5c
Nikita Vokuev <nvokuyev@bk.ru>
2023-08-02 16:32:00 +0300

    запуск бота

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Hello World!")
-a = 1
-b = 2
-print(a+b)
+from aiogram import Bot, Dispatcher, executor, types
+# Import token from cfg.py
+from cfg import TOKEN_API
+
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.message):
+    await message.answer(text=message.text) # Write message
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
