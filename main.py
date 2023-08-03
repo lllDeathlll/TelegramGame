b83e96fb4f7f6ea3c2621b6217075e44dc1e4f2b
Roman Avdeev <lll_Death_lll@outlook.com>
2023-08-03 09:30:53 +0400

    Changes

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from aiogram import Bot, Dispatcher, executor, types
-import random
-import string
-import requests
 # Import token from cfg.py
 from cfg import TOKEN_API
 
@@ -9,14 +6,9 @@ from cfg import TOKEN_API
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
-# Adds random letter on text
-@dp.message_handler()
-async def random(message: types.message):
-    await message.reply(text=f"{await random.choice(set([string.ascii_letters]))}")
-
 # Adds start command with welcome message
 @dp.message_handler(commands=["start"])
-async def help_command(message: types.message):
+async def start_command(message: types.message):
     await message.answer(text="Welcome to our telegram bot!")
     await message.delete()
 
@@ -27,31 +19,12 @@ async def help_command(message: types.message):
 /start - start working with bot
 /help - list of commands
 /description - gives you a description of our bot
-/bitcoin - gives you bitcoin rate
 """)
     
 # Adds description command that gives a description of bot
 @dp.message_handler(commands=["description"])
-async def help_command(message: types.message):
-    await message.reply(text="In our bot you will be able to play a game")
-    
-# Adds bitcoin command that gives you bitcoin rate    
-@dp.message_handler(commands=['bitcoin'])
-async def help_command(message: types.message):
-    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-    response = await requests.get(url)
-    data = response.json()
-
-    await message.reply(text=f"Bitcoin value in USD is: {data['bpi']['USD']['rate']}")
-
-count = 0
- 
-# Adds count command that adds 1 every time
-@dp.message_handler(commands=['count'])
-async def help_command(message: types.message):
-    global count
-    await message.reply(text=f"Count is {count}")
-    count += 1
+async def description_command(message: types.message):
+    await message.reply(text="""In our bot you will be able to play a game called "foobar". To start the game press play button""")
 
 if __name__ == '__main__':
     executor.start_polling(dp)
