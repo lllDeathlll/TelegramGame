514ffe79a72a3cda6b9d83f1c140a37c83669c07
Roman Avdeev <lll_Death_lll@outlook.com>
2023-08-04 15:06:44 +0400

    Added keyboards on start and help command

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 # Import token from cfg.py
 from cfg import TOKEN_API
 
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
+
+# Adds keyboard on start command with auto resizing
+start_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+start_kb.add(KeyboardButton("/help"))
+# Adds keyboard on help command with auto resizing that minimizes on press
+help_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+help_b1 = KeyboardButton("/help")
+help_b2 = KeyboardButton("/description")
+help_kb.add(help_b1).add(help_b2)
     
 # Adds function that completes on bot startup
 async def on_startup(_):
@@ -13,27 +23,37 @@ async def on_startup(_):
 # Adds start command with welcome message
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await message.answer(text="Welcome to our <b>telegram bot!</b>", parse_mode="HTML")
-    await bot.send_sticker(message.from_user.id, sticker="CAACAgUAAxkBAAEJ5u9ky0lSzGPELeYrA1QmXJIK2G3qFgACbwMAAukKyAOvzr7ZArpddC8E")
+    await bot.send_message(chat_id=message.from_user.id,
+                           text="Welcome to our <b>telegram bot!</b>",
+                           parse_mode="HTML",
+                           reply_markup=start_kb)
+    await bot.send_sticker(message.from_user.id,
+                           sticker="CAACAgUAAxkBAAEJ5u9ky0lSzGPELeYrA1QmXJIK2G3qFgACbwMAAukKyAOvzr7ZArpddC8E")
     await message.delete()
 
 # Adds help command that lists all commands
 @dp.message_handler(commands=["help"])
 async def help_command(message: types.Message):
-    await message.reply(text="""
+    await bot.send_message(chat_id=message.from_user.id,
+                           text="""
 <b>/start</b> - <em>start working with bot</em>
 <b>/help</b> - <em>list of commands</em>
 <b>/description</b> - <em>gives you a description of our bot</em>
-""", parse_mode="HTML")
+""",
+                           parse_mode="HTML",
+                           reply_markup=help_kb)
     
 # Adds description command that gives a description of bot
 @dp.message_handler(commands=["description"])
-async def description_command(message: types.Message):
+async def desc_command(message: types.Message):
     await message.reply(text="""In our bot you will be able to play a game called "foobar". To start the game press play button""")
+    await bot.send_photo(caption="""<a href="https://t.me/poncheg11">Никита Вокуев</a>""", parse_mode="HTML",
+                         photo="https://i.mycdn.me/i?r=AzFIxPtkV78jcmdRfpoIOyaJnpzDvEoFL64PcQU7Y2RAeDO7EuABt8t6j6ROsg0SwU4",
+                         chat_id=message.chat.id)
     
 # Returns sticker id
 @dp.message_handler(content_types=["sticker"])
-async def sticker_id_command(message: types.Message):
+async def sticker_id(message: types.Message):
     await message.answer(message.sticker.file_id)
     
 if __name__ == '__main__':
